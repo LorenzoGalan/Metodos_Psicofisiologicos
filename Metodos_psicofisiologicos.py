7792b22fa185107310041b1ad5bc4523424022ca
LorenzoGalan <69258498+LorenzoGalan@users.noreply.github.com>
2021-05-15 19:35:16 -0300

    Add files via upload

---
+++ Metodos_psicofisiologicos.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 13:02:21 2021
+
+@author: Sergio Profumo
+"""
+
+
+
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sy
+from pprint import pprint
+
+pupil= np.loadtxt("pupil_positions.csv", usecols= (6), delimiter=',', skiprows=1)
+data = np.loadtxt('Flor.csv', usecols = (0),  delimiter=',')
+
+pprint(data)
+
+agregar_1= lambda x:x+10
+
+list(map(agregar_1,pupil))
+
+df=pd.DataFrame({"sujeto1":list(map(agregar_1,pupil))})
+plt.plot(df)
+df.to_csv("sujeto1.csv")
+#%%
+def agregar_1(numero):
+    return numero + 10
+
+agregar_1(1)
